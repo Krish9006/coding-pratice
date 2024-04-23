930b5a6ff1675d0440815634066837e289eb8c7f
krish <guptashanu341@gmail.com>
2024-04-23 12:02:10 +0530

    seven commit

---
+++ 3120. Count the Number of Special Characters I.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        set_a=set(word)
+        count=0
+        for i in set_a:
+            if  i.upper() in set_a and i.lower() in set_a:
+               count+=1
+            
+        return count//2
+            
+        
