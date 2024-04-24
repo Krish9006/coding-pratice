883dc991a5f8b53e23f5072630a4b63d7c786121
krish <guptashanu341@gmail.com>
2024-04-24 12:16:19 +0530

    ten commit

--- class complex.py
+++ class complex.py
@@ -4,9 +4,25 @@ class complex:
         self.img=img
     def __add__(self,b):
         return complex(self.real + b.real ,self.img + b.img)
+    
+    def __sub__(self,b):
+        return complex(self.real - b.real ,self.img - b.img)
+    def __mul__(self,b):
+        return complex(self.real  b.*real ,self.img * b.img)
+    def __divide__(self,b):
+        return complex(self.real  b./real ,self.img / b.img)
+
+
 a=complex(5,6)
 b=complex(4,6)
 c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+
 print(c.real,c.img)
+print(d.real,d.img)
+print(e.real,e.img)
         
         
