de6755f96aec83fd8e73668553c4afdb0bf3663e
krish <guptashanu341@gmail.com>
2024-04-26 12:55:32 +0530

    14 commit

---
+++ 2d array.java
@@ -0,0 +1,23 @@
+package TwoDimensionalArray;
+import java .util.scanner;
+public class 2darray;
+public static void main(string[] agrs){
+    scanner sc = new scanner(system.in);
+    int [][] arr= new int[4][2];
+    for(int i=0;i<4;i++){
+        for(j=0;j<2;j++){
+            arr[i][j]=sc.nextINT();
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+            system.out.print(arr[i][]+" ");
+            system.out.println();
+        }
+    }
+}
+
+
+
+
+}
