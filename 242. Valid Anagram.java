1e1be864af176f62edb4fe3723c73bec448f6dfe
krish <guptashanu341@gmail.com>
2024-05-05 20:32:49 +0530

    50 commit

--- 242. Valid Anagram.java
+++ 242. Valid Anagram.java
@@ -2,4 +2,4 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         sorted_s = sorted(s)
         sorted_t = sorted(t)
-        return sorted_s==sorted_y   
+        return sorted_s==sorted_t
