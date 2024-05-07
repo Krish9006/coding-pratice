b07c840e9f332e2c241873672e5901f97d617168
krish <guptashanu341@gmail.com>
2024-05-07 20:54:02 +0530

    55 commit

---
+++ Long Long Sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    long long int sum = 0;
+    long long int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        sum+=arr[i];
+    }
+
+    printf("%lld\n", sum);
+
+    return 0;
+}
