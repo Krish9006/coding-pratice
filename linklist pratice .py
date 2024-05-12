1117cbc536d65c6b36349352c4e155a04662f4b2
krish <guptashanu341@gmail.com>
2024-05-12 12:13:53 +0530

    74 commit

--- linklist pratice .py
+++ linklist pratice .py
@@ -1,38 +1,43 @@
 class Node:
-    def __init__(self,item= None,data=None):
-        self.item=item
-        self.data=data
-class SSL: 
-    def __init__(self,start=None):
-        self.start =start 
-        def __is_empty(Self):
-            return self.start==None
-    def __insertFirst(self,data):
-        n=Node(data,self.start)
-        self.start=n
-    def __insertlast(self,data):
-        n=Node(data)
-        if  not self.start .is_empty():
-            temp=self.start
+    def __init__(self, item=None, data=None):
+        self.item = item
+        self.data = data
+
+class SSL:
+    def __init__(self, start=None):
+        self.start = start
+
+    def is_empty(self):
+        return self.start is None
+
+    def insertFirst(self, data):
+        n = Node(data, self.start)
+        self.start = n
+
+    def insertlast(self, data):
+        n = Node(data)
+        if self.is_empty():
+            self.start = n
+        else:
+            temp = self.start
             while temp.next is not None:
-                temp=temp.next
-                temp=next=n
-            else:
-                self.start=n
-    def __search(self,data):
-        if not self.start.is_empty():
-            temp=self.start
-            while temp.next is not None:
-                if temp.item==data:
+                temp = temp.next
+            temp.next = n
+
+    def search(self, data):
+        if not self.is_empty():
+            temp = self.start
+            while temp is not None:
+                if temp.item == data:
                     return temp
-                temp=temp.next
+                temp = temp.next
             else:
                 return None
         else:
-            return Nono
-        
+            return None
 
-
-        
-
-mylist=SSL()
+mylist = SSL()
+mylist.insertFirst(1)
+mylist.insertlast(7)
+mylist.insertFirst(19)
+print(mylist)
