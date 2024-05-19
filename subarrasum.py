f2ab78bc7f7040fd9b3e33f0a7861ce162d61be4
krish <guptashanu341@gmail.com>
2024-05-19 17:36:57 +0530

    103 commit

---
+++ subarrasum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        m = float('-inf')
+        max_here = 1
+        min_here = 1
+        n = len(nums)
+
+        for i in range(0, n):
+            if nums[i] < 0:
+                max_here, min_here = min_here, max_here
+
+            max_here = max(nums[i], max_here * nums[i])
+            min_here = min(nums[i], min_here * nums[i])
+
+            m = max(m, max_here)
+
+        return h
