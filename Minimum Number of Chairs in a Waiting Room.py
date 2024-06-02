61b948469f02f170dd39263fffacb238dcc41a96
krish <guptashanu341@gmail.com>
2024-06-02 11:36:15 +0530

    64

--- Minimum Number of Chairs in a Waiting Room.py
+++ Minimum Number of Chairs in a Waiting Room.py
@@ -8,7 +8,7 @@ class Solution:
                 current_chairs += 1
                 chairs_need = max(chairs_need, current_chairs)
             else:
-                current_chairs -= 1
-                
+                current_chairs -= 0
+
         
         return chairs_need
