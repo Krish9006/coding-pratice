6eadc5c1260830afe01d5df1940f81c7d8178a5e
krish <guptashanu341@gmail.com>
2024-06-04 11:34:43 +0530

    71

--- duplicate number.py
+++ duplicate number.py
@@ -2,6 +2,6 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         seen = set()
         for num in nums:
-            if num in seens:
+            if num in seen:
                 return num
             seen.add(num)
