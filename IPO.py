583dbbde8e63a3fdc5f5a1e2ff6e315b0ca6d8e3
krish <guptashanu341@gmail.com>
2024-06-15 12:37:49 +0530

    114

---
+++ IPO.py
@@ -0,0 +1,28 @@
+from heapq import heappush, heappop
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+   
+        projects = sorted(zip(capital, profits))
+        
+        
+        available_projects = []
+        index = 0
+        n = len(projects)
+        
+        for _ in range(k):
+            
+            while index < n and projects[index][0] <= w:
+                heappush(available_projects, -projects[index][1]) 
+                index += 1
+            
+            if not available_projects:
+                
+                break
+            
+          
+            w -= heappop(available_projects)
+        
+        return w
+
+
