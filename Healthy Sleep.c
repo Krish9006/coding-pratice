f402a7114a3a0bb45601e2a398ccdb10e4d23cdd
krish <guptashanu341@gmail.com>
2024-06-16 19:52:53 +0530

    118

---
+++ Healthy Sleep.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int h;
+	scanf("%d",&h);
+	if(h<8){
+	        printf("\nless");
+	}
+	else if(h>8){
+	        printf("\nmore");
+	}
+	else{
+	        printf("perfect");
+	}
+
+}
+
