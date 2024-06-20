010ceb6edd79146e6209f62fe4e67d06f81556c6
krish <guptashanu341@gmail.com>
2024-06-20 21:58:32 +0530

    130

---
+++ Magnetic Force Between Two Balls.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int low = 1;
+        int high = position[position.length - 1] - position[0];
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canPlaceBalls(position, m, mid)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return high;
+    }
+    
+    private boolean canPlaceBalls(int[] position, int m, int minForce) {
+        int count = 1;
+        int lastPos = position[0];
+        
+        for (int i = 1; i < position.length; i++) {
+            if (position[i] - lastPos >= minForce) {
+                count++;
+                lastPos = position[i];
+                if (count == m) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
