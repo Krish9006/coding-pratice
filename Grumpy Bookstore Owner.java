c1f3aff6e85fdc805a926664d2ee7c3916545f54
krish <guptashanu341@gmail.com>
2024-06-21 20:02:20 +0530

    133

---
+++ Grumpy Bookstore Owner.java
@@ -0,0 +1,39 @@
+public class Grumpy Bookstore Owner {
+    
+}
+class Solution {
+
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int n = customers.length;
+        int unrealizedCustomers = 0;
+
+       
+        for (int i = 0; i < minutes; i++) {
+            unrealizedCustomers += customers[i] * grumpy[i];
+        }
+
+        int maxUnrealizedCustomers = unrealizedCustomers;
+
+        for (int i = minutes; i < n; i++) {
+
+            unrealizedCustomers += customers[i] * grumpy[i];
+            unrealizedCustomers -= customers[i - minutes] * grumpy[i - minutes];
+
+            
+            maxUnrealizedCustomers = Math.max(
+                maxUnrealizedCustomers,
+                unrealizedCustomers
+            );
+        }
+
+
+        int totalCustomers = maxUnrealizedCustomers;
+
+        for (int i = 0; i < customers.length; i++) {
+            totalCustomers += customers[i] * (1 - grumpy[i]);
+        }
+
+        
+        return totalCustomers;
+    }
+}
