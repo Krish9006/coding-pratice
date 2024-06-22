8b7946d4388bc89e950a3c08e772bc41093b3880
krish <guptashanu341@gmail.com>
2024-06-22 18:12:17 +0530

    137

---
+++ ount Number of Nice Subarrays.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int count = 0;
+        int left = 0;
+        int right = 0;
+        int oddCount = 0;
+        int subarrayCount = 0;
+
+        while (right < nums.length) {
+            if (nums[right] % 2 == 1) {
+                oddCount++;
+                subarrayCount = 0;
+            }
+
+            
+            while (oddCount == k) {
+                if (nums[left] % 2 == 1) {
+                    oddCount--;
+                }
+                left++;
+                subarrayCount++;
+            }
+
+            count += subarrayCount;
+            right++;
+        }
+
+        return count;
+    }
+}
