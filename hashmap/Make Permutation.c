541a3d8aa7aef5287f8c8bea70e52c4fa69fbdb2
krish <guptashanu341@gmail.com>
2024-06-27 20:05:38 +0530

    155

---
+++ hashmap/Make Permutation.c
@@ -0,0 +1,19 @@
+def can_make_permutation(N, A):
+    used = set()
+    for num in A:
+        candidate = num
+        while candidate <= N and candidate in used:
+            candidate += 1
+        if candidate > N:
+            return False
+        used.add(candidate)
+    return True
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+    if can_make_permutation(N, A):
+        print("YES")
+    else:
+        print("NO")
